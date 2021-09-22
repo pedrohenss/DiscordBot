ca51f5d46b02ef8f7c9565f8a2a0a83bcab0b216
Pedro Henrique Santos <pxdr21@hotmail.com>
2021-09-22 20:28:49 -0300

    primeiros comandos do bot

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot ('!')
+
+@bot.event
+async def on_ready():
+    print (f'Estou pronto e conectado como {bot.user}')
+
+@bot.event
+async def on_message (message):
+    if message.author == bot.user:
+        return
+
+    if 'série b' in message.content:
+        await message.channel.send (f'Por favor {message.author.name} pare, os cruzeirenses, vascaínos e botafoguenses deste Discord já sofrem demais!')
+
+    await bot.process_commands(message)
+
+@bot.command (name='vasco')
+async def send_vasco(ctx):
+    name = ctx.author.name
+
+    response = "VASCO DA GAMA E NADA MAIS, " + name
+
+    await ctx.send (response)
 
 
+
+bot.run('ODkwMzQ1MjkyNjIzNDA5MjMy.YUucqw.QMfv7feo46XYdobVVDTlZ2LIFqk')
