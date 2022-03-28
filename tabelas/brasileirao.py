2503e73fdde589b8f0f4144396e30bd275853e50
Pedro Santos <pxdr21@hotmail.com>
2022-03-28 15:14:40 -0300

    Update brasileirao.py

--- tabelas/brasileirao.py
+++ tabelas/brasileirao.py
@@ -3,14 +3,14 @@ from requests_html import HTMLSession
 ## Scraping Tabela Brasileirão
 
 session = HTMLSession()
-requisicao = session.get('https://www.goal.com/br/brasileir%C3%A3o-s%C3%A9rie-a/tabela-de-classifica%C3%A7%C3%A3o/scf9p4y91yjvqvg5jndxzhxj')
+requisicao = session.get('https://www.eurosport.com/football/brazilian-serie-a/standing.shtml')
 
-nome_times = requisicao.html.find('.widget-match-standings__team--full-name')
-points = requisicao.html.find('.widget-match-standings__pts')
+nome_times = requisicao.html.find('.text')
+points = requisicao.html.find('.standing-table__cell-value--main')
 tabela = []
 
 for item in range(0, 20):
-    tabela.append(points[item + 1].text + ' - ' + nome_times[item].text + ' ' + '\n' )
+    tabela.append(points[item].text + ' - ' + nome_times[item].text + ' ' + '\n' )
 
 tabela = (''.join(map(str,tabela)))
 print(tabela)
